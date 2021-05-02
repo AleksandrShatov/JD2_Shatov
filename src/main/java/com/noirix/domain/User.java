f82c88df71d922ffa90995025111c88f03dd92fc
AleksandrShatov <shatov.alexman@gmail.com>
2021-05-02 18:15:51 +0300

    JDBC Initial commit

---
+++ src/main/java/com/noirix/domain/User.java
@@ -0,0 +1,11 @@
+package com.noirix.domain;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String birth_date;
+    private String login;
+    private String weight;
+}
