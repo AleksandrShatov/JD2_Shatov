c6df78e026fa8e0a85f1685a0f8b9dea45e2eec9
AleksandrShatov <shatov.alexman@gmail.com>
2021-05-03 23:21:15 +0300

    Save, findOne test methods

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -4,15 +4,34 @@ import com.noirix.domain.User;
 import com.noirix.repository.UserRepository;
 import com.noirix.repository.impl.UserRepositoryImpl;
 
+import java.sql.Date;
+
 public class Main {
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepositoryImpl();
 
 //        userRepository.findAll().stream().forEach(User::toString);
 
-        for (User user : userRepository.findAll()) {
-            System.out.println(user);
-        }
+        // Find all users
+//        for (User user : userRepository.findAll()) {
+//            System.out.println(user);
+//        }
 
+        // Find one user
+//        try {
+//            System.out.println(userRepository.findOne(5L));
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//        }
+
+        // Save user
+        User user = new User();
+        user.setName("Test2");
+        user.setSurname("Save2");
+        user.setLogin("test_save_2");
+        user.setWeight(120f);
+        user.setBirthDate(new Date(12000000L));
+
+        System.out.println(userRepository.save(user));
     }
 }
---
+++ src/main/java/com/noirix/exception/NoSuchEntityException.java
@@ -0,0 +1,9 @@
+package com.noirix.exception;
+
+public class NoSuchEntityException extends RuntimeException {
+
+    public NoSuchEntityException(String message) {
+        super(message);
+
+    }
+}
