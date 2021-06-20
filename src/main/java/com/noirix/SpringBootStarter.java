1a6aed58075d5e6a574ebbc26be3af46878ce102
AleksandrShatov <shatov.alexman@gmail.com>
2021-06-20 19:35:08 +0300

    Swagger API initial commit

--- src/main/java/com/noirix/SpringBootStarter.java
+++ src/main/java/com/noirix/SpringBootStarter.java
@@ -3,17 +3,21 @@ package com.noirix;
 import com.noirix.beans.ApplicationBeans;
 import com.noirix.beans.GitSecretConfig;
 import com.noirix.beans.SecurityConfig;
+import com.noirix.beans.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.noirix")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableWebMvc
+@EnableSwagger2
 @Import({
         ApplicationBeans.class,
+        SwaggerConfig.class,
         SecurityConfig.class,
         GitSecretConfig.class})
 public class SpringBootStarter {
