2903caf717200bdd55a11cd2dd204ae5f5935aa2
AleksandrShatov <shatov.alexman@gmail.com>
2021-07-04 14:03:28 +0300

    Hibernate initial

---
+++ src/main/java/com/noirix/domain/Gender.java
@@ -0,0 +1,6 @@
+package com.noirix.domain;
+
+public enum Gender {
+
+    MALE, FEMALE, NOT_SELECTED
+}
---
+++ src/main/java/com/noirix/domain/hibernate/HibernateUser.java
@@ -0,0 +1,41 @@
+package com.noirix.domain.hibernate;
+
+
+import com.noirix.domain.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class HibernateUser {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Gender gender = Gender.NOT_SELECTED;
+
+    @Column
+    private String login;
+
+    @Column
+    private Float weight;
+
+    @Column
+    private String password;
+}
